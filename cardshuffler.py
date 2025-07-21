619ac363454473cfe1975b171db5d9a25c06b09a
Jack <111350133+jackGuzzie@users.noreply.github.com>
2025-07-21 17:58:20 +0800

    Version 3
    - Some optimisations
    - Added ability to sort and print out deck

--- cardshuffler.py
+++ cardshuffler.py
@@ -5,7 +5,7 @@ ranks = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
 face = ["K","Q","J"]
 suits = ["S","H","C","D"]
 
-# Reset Deck
+# Generate Deck Functions
 def resetDeck(choice):
     match choice:
         case 1:
@@ -29,12 +29,7 @@ def genRegular():
             card = j + i
             cardsUnshuffled.append(card)
 
-    # Write deck to file
-    file = open("deck.txt","w")
-    for i in range(0,len(cardsUnshuffled)):
-        file.write(cardsUnshuffled[i]+"\n")
-    file.close()
-
+    writeDeck(cardsUnshuffled)
 
 def genCheckered():
     cardsUnshuffled = []
@@ -54,12 +49,7 @@ def genCheckered():
             card = rank + suit
             cardsUnshuffled.append(card)
 
-    # Write deck to file
-    file = open("deck.txt","w")
-    for i in range(0,len(cardsUnshuffled)):
-        file.write(cardsUnshuffled[i]+"\n")
-    file.close()
-
+    writeDeck(cardsUnshuffled)
 
 def genAbandoned():
     cardsUnshuffled = []
@@ -76,12 +66,7 @@ def genAbandoned():
                 card = rank + suit
                 cardsUnshuffled.append(card)
 
-    # Write deck to file
-    file = open("deck.txt","w")
-    for i in range(0,len(cardsUnshuffled)):
-        file.write(cardsUnshuffled[i]+"\n")
-    file.close()
-
+    writeDeck(cardsUnshuffled)
 
 def genErratic():
     cardsUnshuffled = []
@@ -94,28 +79,89 @@ def genErratic():
         card = rank + suit
         cardsUnshuffled.append(card)
 
+    writeDeck(cardsUnshuffled)
+
+# File Functions
+def writeDeck(cards):
     # Write deck to file
     file = open("deck.txt","w")
-    for i in range(0,len(cardsUnshuffled)):
-        file.write(cardsUnshuffled[i]+"\n")
+    for i in range(0,len(cards)):
+        file.write(cards[i]+"\n")
     file.close()
 
-def shuffleDeck():
-    global deckSize
-    # If file does not exist then create it
-    cardsUnshuffled = []
+def readDeck():
+    cards = []
     try:
         file = open("deck.txt", "r")
     except:
         genRegular()
         file = open("deck.txt", "r")
+    
     card = file.readline().strip("\n")
     # Read deck from file
     while card != "":
-        cardsUnshuffled.append(card)
+        cards.append(card)
         card = file.readline().strip("\n")
-    deckSize = len(cardsUnshuffled)
     file.close()
+    return cards
+
+def printDeck(cards):
+    rankNames = ["Ace of ","King of ","Queen of ","Jack of ","Ten of ","Nine of ","Eight of ","Seven of ","Six of ","Five of ","Four of ","Three of ","Two of "]
+    suitNames = ["Spades","Hearts","Clubs","Diamonds"]
+    # String
+    for card in cards:
+        cardName = ""
+        for rank in ranks:
+            if str(card).startswith(rank):
+                cardName = cardName + rankNames[ranks.index(rank)]
+        for suit in suits:
+            if str(card).endswith(suit):
+                cardName = cardName + suitNames[suits.index(suit)]
+        print(cardName)
+
+# Sorting/Shuffle Functions
+def parse_card(card):
+    for rank in ranks:
+        if str(card).startswith(rank):
+            suit = card[len(rank):]
+            return rank, suit
+    return None, None  # fallback
+
+def sortDeck(choice):
+    match choice:
+        case 1:
+            sortDeckRank()
+        case 2:
+            sortDeckSuit()
+        case _:
+            print("Invalid choice.")
+
+def sortDeckSuit():
+    global deckSize
+    # If file does not exist then create it
+    cardsUnsorted = readDeck()
+
+    def card_key(card):
+        rank, suit = parse_card(card)
+        return (suits.index(suit), ranks.index(rank))
+    cardsSorted = sorted(cardsUnsorted, key=card_key)
+    writeDeck(cardsSorted)
+
+def sortDeckRank():
+    global deckSize
+    # If file does not exist then create it
+    cardsUnsorted = readDeck()
+
+    def card_key(card):
+        rank, suit = parse_card(card)
+        return (ranks.index(rank), suits.index(suit))
+    cardsSorted = sorted(cardsUnsorted, key=card_key)
+    writeDeck(cardsSorted)
+
+def shuffleDeck():
+    global deckSize
+    # If file does not exist then create it
+    cardsUnshuffled = readDeck()
 
     # Shuffle cards
     cardsShuffled = []
@@ -134,59 +180,14 @@ def shuffleDeck():
         # Place into deck
         cardsIndexed.append(shuffle)
         cardsShuffled.append(cardsUnshuffled[shuffle])
-    printShuffle(cardsShuffled)
+    writeDeck(cardsShuffled)
 
-
-def printShuffle(cardsShuffled):
-    # Print shuffled card order
-    for i in range(0,deckSize):
-        cardName = ""
-        # Rank
-        match cardsShuffled[i][0]:
-            case "A":
-                cardName = cardName + "Ace of "
-            case "K":
-                cardName = cardName + "King of "
-            case "Q":
-                cardName = cardName + "Queen of "
-            case "J":
-                cardName = cardName + "Jack of "
-            case "1":
-                cardName = cardName + "Ten of "
-            case "9":
-                cardName = cardName + "Nine of "
-            case "8":
-                cardName = cardName + "Eight of "
-            case "7":
-                cardName = cardName + "Seven of "
-            case "6":
-                cardName = cardName + "Six of "
-            case "5":
-                cardName = cardName + "Five of "
-            case "4":
-                cardName = cardName + "Four of "
-            case "3":
-                cardName = cardName + "Three of "
-            case "2":
-                cardName = cardName + "Two of "
-        # Suit
-        match cardsShuffled[i][-1]:
-            case "S":
-                cardName = cardName + "Spades"
-            case "H":
-                cardName = cardName + "Hearts"
-            case "C":
-                cardName = cardName + "Clubs"
-            case "D":
-                cardName = cardName + "Diamonds"
-        print(cardName)
-
-
+# Menu
 def showMenu():
-    print("\nMenu:\n1. Reset Deck\n2. Shuffle Deck\n3. Close")
+    print("\nMenu:\n1. Reset Deck\n2. Shuffle Deck\n3. Sort Deck\n4. Print Deck\n5. Close")
     match int(input()):
         case 1:
-            print("\nMenu:\n1. Regular\n2. Checkered\n3. Abandoned\n4. Erratic")
+            print("\nDeck:\n1. Regular\n2. Checkered\n3. Abandoned\n4. Erratic")
             choice = int(input())
             resetDeck(choice)
         case 2:
@@ -194,6 +195,13 @@ def showMenu():
             shuffleDeck()
         case 3:
             print("\n")
+            print("\nSort:\n1. Rank\n2. Suit")
+            choice = int(input())
+            sortDeck(choice)
+        case 4:
+            print("\n")
+            printDeck(readDeck())
+        case 5:
             global menu
             menu = False
         case _:
